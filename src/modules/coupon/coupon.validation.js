d55ffe3380f89cac0c76d98da3bf44c8594c8781
mohamed_orabi <morabi@nadeer.com>
2023-07-19 15:19:34 +0300

    k

--- src/modules/coupon/coupon.validation.js
+++ src/modules/coupon/coupon.validation.js
@@ -1,19 +1,14 @@
 import joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
  export const createCoupon = joi.object({
-    name: joi.string().required(),
-    image: joi.object(),
-    discount: joi.number().required(),
-    expiryDate: joi.date().required(),
-    usedBy: generalFields.id.required(),
-    createdBy: generalFields.id.required()
+    name: joi.string().min(2).max(25).required(),
+    image: generalFields.file,
+    discount: joi.number().positive().min(1).max(100).required(),
+    expiryDate: joi.number().positive().min(1).max(100).required()
 }).required();
  export const updateCoupon = joi.object({
-    couponId: generalFields.id.required(),
-    name: joi.string(),
-    image: joi.object(),
+    couponId: generalFields.id,
+    name: joi.string().min(2).max(25),
+    image: generalFields.file,
     discount: joi.number(),
-    expiryDate: joi.date(),
-    usedBy: generalFields.id,
-    createdBy: generalFields.id
 }).required();
