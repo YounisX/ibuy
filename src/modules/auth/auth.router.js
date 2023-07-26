9d9cb12edc536d35a96e08d4cd594ed445496491
mohamed_orabi <morabi@nadeer.com>
2023-07-26 16:43:47 +0300

    done

--- src/modules/auth/auth.router.js
+++ src/modules/auth/auth.router.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import *as authController from '../auth/controller/registration.js'
+import * as validators from './auth.validation.js'
+import { validation } from "../../middleware/validation.js";
 const router = Router()
 
 
-router.post('/',authController.signUp);
+router.post('/signup',validation(validators.signup),authController.signup);
+router.post('/login',authController.login);
+router.get('/confirmEmail/:token',validation(validators.confirmEmail),authController.confirmEmail)
+router.get('/newConfirmEmail/:token',validation(validators.confirmEmail),authController.newConfirmEmail);
+
 
 export default router
---
+++ src/modules/auth/auth.validation.js
@@ -0,0 +1,11 @@
+import joi from "joi";
+import { generalFields } from './../../middleware/validation.js';
+
+export const signup = joi.object({
+    userName:joi.string().min(2).max(100).required(),
+    email:generalFields.email,
+    password:generalFields.password
+}).required()
+export const confirmEmail = joi.object({
+    token:joi.string().required()
+}).required()
