467934a6079a174208961863fff79835f1be36e2
mohamed_orabi <morabi@nadeer.com>
2023-08-03 15:52:38 +0300

    s

--- DB/model/Product.model.js
+++ DB/model/Product.model.js
@@ -1,4 +1,4 @@
-import mongoose, { Types, model } from "mongoose";
+import mongoose, { Schema, Types, model } from "mongoose";
 
 const productSchema = new Schema({
     _id: {
--- src/modules/product/controller/product.js
+++ src/modules/product/controller/product.js
@@ -3,6 +3,8 @@ import categoryModel from "./../../../../DB/model/Category.model.js";
 import brandModel from "./../../../../DB/model/Brand.model.js";
 import slugify from "slugify";
 import cloudinary from './../../../utils/cloudinary.js';
+import { nanoid } from "nanoid";
+import productModel from './../../../../DB/model/Product.model.js';
 
 
 
@@ -11,8 +13,7 @@ export const createProduct = async (req, res, next) => {
 
   //checking if this products belongs to a category
   const category = await categoryModel.findOne({
-    _id: subCategoryId,
-    catergoryId,
+    _id: subCategoryId,catergoryId,
   });
   if (!category) {
     return next(new Error("invalid category or subcategory "), { cause: 400 });
@@ -28,8 +29,22 @@ export const createProduct = async (req, res, next) => {
 
   req.body.finalPrice = price - (price * discount||0) / 100; //finding if there's a
                                 // discount and apply final price
-  const {secure_url,public_id} = await cloudinary.uploader.upload(req.file.path,{folder:`/${process.env.APP_NAME}/Category/SubCategory/Products`})
+req.body.customId = nanoid();
+  const {secure_url,public_id} = await cloudinary.uploader.upload(req.file.path,{folder:`/${process.env.APP_NAME}/Category/SubCategory/Products/${req.body.customId}`})
+  req.body.mainImage = {secure_url,public_id} ; 
 
-
+if(req.files){
+    req.body.subImages=[];
+    for (const file of req.files) {
+  const {secure_url,public_id} = await cloudinary.uploader.upload(file.path,{folder:`/${process.env.APP_NAME}/Category/SubCategory/Products/${req.body.customId}`})
+        req.body.subImages.push({secure_url,public_id})
+    }
+}
+req.body.createdBy = req.body._id;
+const product = await productModel.create(req.body);
+if(!product){
+    return next(new Error("fail to create product"), { cause: 400 });
+}
+return res.json({product})
 
 };
