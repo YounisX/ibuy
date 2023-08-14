6682743f5842dff23817341c40e187392e95a30f
mohamed_orabi <morabi@nadeer.com>
2023-08-14 12:16:41 +0300

    product update

--- src/modules/product/controller/product.js
+++ src/modules/product/controller/product.js
@@ -51,3 +51,48 @@ if(!product){
 return res.json({product})
 
 })
+
+
+
+export const updateCategory = AsyncHandler( async (req, res, next) => {
+  const { name, catergoryId, subCategoryId, brandId,price,discount } = req.body;
+
+  //checking if this products belongs to a category
+  const category = await categoryModel.findOne({
+    subCategoryId,catergoryId
+  });
+  if (!category) {
+    return next(new Error("invalid category or subcategory "), { cause: 400 });
+  }
+
+  //checking if that product belongs to brand
+  const brand = await brandModel.findOne({ _id: brandId });
+  if (!brand) {
+    return next(new Error("invalid Brand "), { cause: 400 });
+  }
+
+  req.body.slug = slugify(name, { lower: true, replacement: "-", trim: true });
+
+  req.body.finalPrice = Number.parseFloat(price - (price * discount||0) / 100).toFixed(2) //finding if there's a
+                                // discount and apply final price
+req.body.paymentPricre = req.body.finalPrice ;                      
+
+req.body.cutomId = nanoid();
+  const {secure_url,public_id} = await cloudinary.uploader.upload(req.files.mainImage[0].path,{folder:`/${process.env.APP_NAME}/Category/SubCategory/Products/${req.body.customId}`})
+  req.body.mainImage = {secure_url,public_id} ; 
+  
+if(req.files.subImages){
+    req.body.subImages=[];
+    for (const file of req.files.subImages) {
+  const {secure_url,public_id} = await cloudinary.uploader.upload(file.path,{folder:`/${process.env.APP_NAME}/Category/SubCategory/Products/${req.body.customId}`})
+        req.body.subImages.push({secure_url,public_id})
+    } 
+}
+req.body.createdBy = req.user._id;
+const product = await productModel.create(req.body);
+if(!product){ 
+    return next(new Error("fail to create product"), { cause: 400 });
+}
+return res.json({product})
+
+})
