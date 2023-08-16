905f7f142f6dc3b1b1d09d2f6106137d3318ceb0
mohamed_orabi <morabi@nadeer.com>
2023-08-16 14:34:46 +0300

    cart and chalk package

--- DB/model/Cart.model.js
+++ DB/model/Cart.model.js
@@ -1,22 +1,34 @@
-import mongoose,{model,Schema,Types} from "mongoose";
+import mongoose, { model, Schema, Types } from "mongoose";
 
-const cartSchema = new Schema({
-
+const cartSchema = new Schema(
+  {
     userId: {
-        type: Types.ObjectId,
-        required: true,
-        unique:true
+      type: Types.ObjectId,
+      required: true,
+      unique: true,
     },
-    products:[{
-        productId:{
+note:String,
+    products: [
+      {
+        name: { type: String, required: true },
+        productId: { type: Types.ObjectId, ref: "Product", required: true },
+        quantity: { type: Number, required: true, default: 1 },
+        unitPrice: { type: Number, default: 1, required: true },
+        finalPrice: { type: Number, default: 1, required: true },
+      },
+    ],
+subtotal:{type:Number,required:true,default:1}
+,
+    couponId:{type:Types.ObjectId,ref:"Coupon"},
+    totalPrice:{type:Number,required:true,default:1},
+    status:{type:String,default:"placed",enum:["waitPayment","placed","cancelled","delivered","rejected","onWay"]},
+    paymentType:{type:String,default:"cash",enum:["cash","card"]},
+    reason:String,        
 
-            type:Types.ObjectId, ref:'Product',required:true
-            
-        },
-        quantity:{type:Number,required : true , default:1}
-    }]
-    }
-, { timestamps: true });
+  },
+  
+  { timestamps: true }
+);
 
-const CartModel = mongoose.models.Cart||model('Cart',cartSchema);
-export default CartModel; 
+const CartModel = mongoose.models.Cart || model("Cart", cartSchema);
+export default CartModel;
