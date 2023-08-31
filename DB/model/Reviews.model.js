d92e74398434853b4dbda6a5519206cffa8f912b
mohamed_orabi <morabi@nadeer.com>
2023-08-31 15:01:15 +0300

    cancel order and update order,review Model

--- DB/model/Reviews.model.js
+++ DB/model/Reviews.model.js
@@ -1,15 +1,23 @@
+import mongoose, { Schema, Types, model } from "mongoose";
+
 const reviewSchema = new Schema({
-    title: {
+comment: {
         type: String,
         required: true
     },
-    description: {
-        type: String,
+productId: {
+type: Types.ObjectId,ref: "Product",
         required: true
     },
     rating: {
         type: Number,
-        required: true
+        required: true,
+        min: 1,
+        max:5
+    },orderId:{
+        type:Types.ObjectId,
+        ref:'Order',
+        required: true,
     },
     createdBy: {
         type: Types.ObjectId,
